dc844a390e1022d9737b992874e06eff623d5811
Thomas Blume <Thomas.Blume@suse.com>
2023-01-20 10:19:13 +0100

    feat(crypt): remove crypt keyfile before entering emergency shell

--- modules.d/98dracut-systemd/module-setup.sh
+++ modules.d/98dracut-systemd/module-setup.sh
@@ -53,4 +53,37 @@ install() {
     inst_simple "$moddir/dracut-tmpfiles.conf" "$tmpfilesdir/dracut-tmpfiles.conf"
 
     inst_multiple sulogin
+
+    if [ -f "$dracutsysrootdir"/etc/crypttab ]; then
+        remove_cryptkeyfiles
+    fi
+}
+
+remove_cryptkeyfiles() {
+    if [ -z "$keep_cryptkeyfiles" ]; then
+       while read -r _mapper _ _keyfile _o || [ -n "$_mapper" ]; do
+            [[ $_mapper = \#* ]] && continue
+            [[ $_o =~ x-initrd.attach ]] || continue
+            # select entries with password files
+            [[ -f $_keyfile ]] || _keyfile=${_keyfile%:*}
+            [[ -f $_keyfile ]] || _keyfile=/run/cryptsetup-keys.d/$_mapper.key
+            [[ -f $_keyfile ]] || _keyfile=/etc/cryptsetup-keys.d/$_mapper.key
+            [[ $keyfiles =~ $_keyfile ]] || keyfiles+="$_keyfile "
+       done < "$dracutsysrootdir"/etc/crypttab
+
+       if [[ -n $keyfiles ]]; then
+            sed -i "N;/and attach it to a bug report./s/echo$/echo \n\
+            echo 'PLEASE NOTE:'\n\
+            echo 'The keyfiles for automatic decryption of the filesystems are removed from the'\n\
+            echo 'emergency shell due to security constraints.'\n\
+            echo 'Use the --keep-cryptkeyfiles parameter at initrd creation time to suppress this'\n\
+            echo 'behaviour.'\n\
+            echo ''\n/" $initdir/bin/dracut-emergency
+        fi
+
+        for file in $keyfiles; do
+            _keyf+="    rm -f $file\n"
+        done
+        sed -i "s#^.*sulogin -e#$_keyf    exec sulogin -e#" $initdir/bin/dracut-emergency
+    fi
 }
